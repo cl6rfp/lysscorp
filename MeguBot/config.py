3cd942c0718742af5c822388f961e5380a8dcc27
cl6rfp <86677644+cl6rfp@users.noreply.github.com>
2022-05-28 15:31:48 -0700

    Add files via upload

---
+++ MeguBot/config.py
@@ -0,0 +1,12 @@
+from MeguBot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID = 808305342 # Su ID de telegram.
+    OWNER_USERNAME = "hatake_sempai" # Su nombre de usuario de telegram.
+    API_KEY = "AAHQ8To-s-RibVY3aFokaJ9dXPtAkUVLA_s" # Su clave api, tal como la proporciona @botfather.
+    SQLALCHEMY_DATABASE_URI = 'postgresql://nombredeusuario:contraseña@localhost:5432/database' # Credenciales de base de datos de muestra.
+    MESSAGE_DUMP = '-721739978' # Algún chat grupal donde su bot este ahí.
+    USE_MESSAGE_DUMP = True
+    SUDO_USERS = [808305342] # Lista de identificadores de usuarios que tienen acceso superusuario al bot.
+    LOAD = []
+    NO_LOAD = ['translation']
