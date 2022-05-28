54393291ed0fe1b6b4014213410035ef5ec09072
cl6rfp <86677644+cl6rfp@users.noreply.github.com>
2022-05-28 15:38:33 -0700

    Update config.py

--- MeguBot/config.py
+++ MeguBot/config.py
@@ -3,7 +3,7 @@ from MeguBot.sample_config import Config
 class Development(Config):
     OWNER_ID = 808305342 # Su ID de telegram.
     OWNER_USERNAME = "hatake_sempai" # Su nombre de usuario de telegram.
-    API_KEY = "AAHQ8To-s-RibVY3aFokaJ9dXPtAkUVLA_s" # Su clave api, tal como la proporciona @botfather.
+    API_KEY = "5331784189:AAHQ8To-s-RibVY3aFokaJ9dXPtAkUVLA_s" # Su clave api, tal como la proporciona @botfather.
     SQLALCHEMY_DATABASE_URI = 'postgresql://nombredeusuario:contraseña@localhost:5432/database' # Credenciales de base de datos de muestra.
     MESSAGE_DUMP = '-721739978' # Algún chat grupal donde su bot este ahí.
     USE_MESSAGE_DUMP = True
